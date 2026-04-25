94fbb54fd317111d2f5bc091d80da3be6c432c05
RezCode <adlifahrezaf@gmail.com>
2026-04-25 14:41:16 +0700

    Refactor data filtering into a function
    Refactored filtering code into a function and added data import.

--- P1,py/Main_2.py
+++ P1,py/Main_2.py
@@ -1,13 +1,20 @@
-from Main_1 import *
+from Main_1 import data_manipulation
 import numpy as np
 
-#Filtering
-mean_temp = np.mean(df['Temperature'])
-std_temp = np.std(df['Temperature'])
-outlier_limit = mean_temp + (3 * std_temp) # 3-Sigma Rule
+def data_filtering():
+    
+    #Import past data
+    df = data_manipulation()
 
-print(f"\nBatas Anomali Suhu: {outlier_limit:.2f}")
-print(f"Jumlah Data Anomali: {np.sum(df['Temperature'] > outlier_limit)}")
+    #Filtering
+    mean_temp = np.mean(df['Temperature'])
+    std_temp = np.std(df['Temperature'])
+    outlier_limit = mean_temp + (3 * std_temp) # 3-Sigma Rule
+
+    print(f"\nBatas Anomali Suhu: {outlier_limit:.2f}")
+    print(f"Jumlah Data Anomali: {np.sum(df['Temperature'] > outlier_limit)}")
+
+    return df, mean_temp, outlier_limit
 
 
 
