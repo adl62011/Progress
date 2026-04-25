62ade3961ac77d0112720fa5412b3c8ba5382081
RezCode <adlifahrezaf@gmail.com>
2026-04-25 14:41:36 +0700

    Update Main_3.py

--- P1,py/Main_3.py
+++ P1,py/Main_3.py
@@ -1,23 +1,29 @@
-from Main_1 import *
-from Main_2 import *
+from Main_2 import data_filtering
 import matplotlib.pyplot as plt
 
-# --- VISUAL MATPLOTLIB ---
+def main():
+    # --- VISUAL MATPLOTLIB ---
 
-plt.figure(figsize=(12, 5))
+    df, mean_temp, outlier_limit = data_filtering()
+    
+    plt.figure(figsize=(12, 5))
 
-# Plot Histogram Temperature for distribution visual
-plt.hist(df['Temperature'], bins=50, color='skyblue', edgecolor='black', alpha=0.7)
-plt.axvline(mean_temp, color='red', linestyle='dashed', linewidth=2, label='Mean')
-plt.axvline(outlier_limit, color='orange', linestyle='dashed', linewidth=2, label='Anomaly Limit')
+    # Plot Histogram Temperature for distribution visual
+    plt.hist(df['Temperature'], bins=50, color='skyblue', edgecolor='black', alpha=0.7)
+    plt.axvline(mean_temp, color='red', linestyle='dashed', linewidth=2, label='Mean')
+    plt.axvline(outlier_limit, color='orange', linestyle='dashed', linewidth=2, label='Anomaly Limit')
 
-plt.title('Distribution of Engine Temperature (100k Data Points)', fontsize=14)
-plt.xlabel('Temperature (C)')
-plt.ylabel('Frequency')
-plt.legend()
-plt.grid(axis='y', alpha=0.3)
+    plt.title('Distribution of Engine Temperature (100k Data Points)', fontsize=14)
+    plt.xlabel('Temperature (C)')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.grid(axis='y', alpha=0.3)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.savefig('Grafik_data_test.png')
+    
+    print("Membuka jendela grafik...") 
+    plt.show()
 
-
+if __name__ == '__main__':
+    main()
